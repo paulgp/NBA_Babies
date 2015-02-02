63ffe5a97cf229af85af574cc6fac889143ee98a
Paul Goldsmith-Pnikham <paulgp@gmail.com>
2015-02-02 11:40:00 -0500

    Initial Commit

---
+++ pull_births.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+def readNames():
+    # take csv file and return list of nba names, with spaces converted to dashes
+    return None
+
+def getRelations(page):
+    # take an HTML page and find all relationships with nonzero children
+    # return list of urls with nonzero children
+    return None
+def getChildren(page):
+    # take an HTML page and find all birthdays and genders for given relationship
+    # return list of dates
+    return None
+
+def main():
+    player_name = "Michael Jordan"
+    player_name = player_name.replace(" ", "-")
+    print player_name
+    
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+

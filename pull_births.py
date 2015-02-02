ebf2b763259b1754ae832217f7eea37a7d19d2cb
Paul Goldsmith-Pnikham <paulgp@gmail.com>
2015-02-02 13:04:21 -0500

    Read in Files

--- pull_births.py
+++ pull_births.py
@@ -1,14 +1,18 @@
 
 import requests
+import time
 from bs4 import BeautifulSoup as bs
+import csv
 
 PROFILE_URL = "http://www.famoushookups.com/site/celebrity_profile.php"
 RELATIONSHIP_URL = "http://www.famoushookups.com/site/relationship_detail.php"
 
 
-def readNames():
+def readNames(f):
     # take csv file and return list of nba names, with spaces converted to dashes
-    return None
+    reader = csv.reader(f)
+    reader.next()
+    return [x[2] for x in reader]
 
 def getRelations(page):
     # take an HTML page and find all relationships with nonzero children
@@ -30,6 +34,8 @@ def getChildren(page):
     soup = bs(page.text)
     kidData = soup.find('tr', attrs={'class':'rel_child_header'})
     birthDates=[]
+    if kidData is None:
+        return ["No Data"]
     for child in  kidData.parent.children:
         try:
             birthDates.extend(child.find_all('td', attrs={'class':'rel_child_col2'}))
@@ -37,10 +43,14 @@ def getChildren(page):
             pass
     return [x.text for x in birthDates]
 def main():
-    player_names = ["Michael Jordan"]
+    #player_names = ["Michael Jordan"]
+    filename = "wikipedia_allstars_clean.csv"
+    with open(filename, 'rb') as f:
+        player_names = readNames(f)
+    
     data = {}    
     for player_name in player_names:
-
+        
         player_name2 = player_name.replace(" ", "-")
         print player_name
 
@@ -54,7 +64,16 @@ def main():
             page = requests.get(relation)
             dateList.extend(getChildren(page))
         data[player_name] = dateList
-    print data
+        
+        #Sleep to not overwhelm the server
+        time.sleep(0.1)  
+
+    with open('data_output.csv', 'wb') as f:
+        writer = writer.csvwriter(f)
+        writer.writerow('Player', 'Birthdate')
+        for key in data.keys():
+            for date in data[key]:
+                writer.writerow([key, date])
 
 if __name__ == "__main__":
     main()

8711e3009234d4a7c3a3d3d4521da913fcaffc6a
Paul Goldsmith-Pnikham <paulgp@gmail.com>
2015-02-02 12:33:13 -0500

    Updated Main Functions
    Get Relationships and Get Children is finished. Works for one player
    right now

--- pull_births.py
+++ pull_births.py
@@ -1,6 +1,9 @@
 
+import requests
+from bs4 import BeautifulSoup as bs
 
-
+PROFILE_URL = "http://www.famoushookups.com/site/celebrity_profile.php"
+RELATIONSHIP_URL = "http://www.famoushookups.com/site/relationship_detail.php"
 
 
 def readNames():
@@ -10,17 +13,48 @@ def readNames():
 def getRelations(page):
     # take an HTML page and find all relationships with nonzero children
     # return list of urls with nonzero children
-    return None
+    soup = bs(page.text)
+    kidData =  soup.find_all('td', attrs={'class':'col_rel_kids'})
+    kidUrlList= []
+    for kid in kidData:
+        try:
+            if int(kid.text) > 0:
+                kidUrlList.append(kid.find('a').get('href'))
+        except ValueError:
+            pass
+    return kidUrlList
+
 def getChildren(page):
     # take an HTML page and find all birthdays and genders for given relationship
     # return list of dates
-    return None
-
+    soup = bs(page.text)
+    kidData = soup.find('tr', attrs={'class':'rel_child_header'})
+    birthDates=[]
+    for child in  kidData.parent.children:
+        try:
+            birthDates.extend(child.find_all('td', attrs={'class':'rel_child_col2'}))
+        except AttributeError:
+            pass
+    return [x.text for x in birthDates]
 def main():
-    player_name = "Michael Jordan"
-    player_name = player_name.replace(" ", "-")
-    print player_name
-    
+    player_names = ["Michael Jordan"]
+    data = {}    
+    for player_name in player_names:
+
+        player_name2 = player_name.replace(" ", "-")
+        print player_name
+
+        payload = {'name' : player_name2}
+        page = requests.get(PROFILE_URL, params=payload)
+
+        relUrlList = getRelations(page)
+        
+        dateList = []
+        for relation in relUrlList:
+            page = requests.get(relation)
+            dateList.extend(getChildren(page))
+        data[player_name] = dateList
+    print data
 
 if __name__ == "__main__":
     main()
